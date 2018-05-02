d5303afff1523e9cad4499fdb505fc8ac8bfa7fb
Cindy <jl7e12@gmail.com>
2018-05-02 15:10:01 +0100

    First fizz test

---
+++ Fizzbuzz/Brain.swift
@@ -0,0 +1,12 @@
+//
+//  Brain.swift
+//  Fizzbuzz
+//
+//  Created by Jia Liu on 02/05/2018.
+//  Copyright © 2018 Cindy Liu. All rights reserved.
+//
+
+import Foundation
+class Brain {
+    
+}
---
+++ FizzbuzzTests/BrainTests.swift
@@ -0,0 +1,30 @@
+//
+//  FizzbuzzTests.swift
+//  FizzbuzzTests
+//
+//  Created by Jia Liu on 02/05/2018.
+//  Copyright © 2018 Cindy Liu. All rights reserved.
+//
+
+import XCTest
+@testable import Fizzbuzz
+
+class FizzbuzzTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(number: 3)
+        XCTAssertEqual(result, true)
+    }
+    
+}
